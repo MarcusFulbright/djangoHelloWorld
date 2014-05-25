86e0eb1d3ab357bdef369ae40dd69dadee96678e
Marcus Fulbright <marcus.fulbright@iostudio.com>
2014-05-25 12:09:38 -0500

    refatored index view to ignore polls with future pub_at dates

--- helloWorld/polls/views.py
+++ helloWorld/polls/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.views import generic
+from django.utils import timezone
 
 from polls.models import Poll, Choice
 
@@ -10,8 +11,10 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_poll_list'
 
     def get_queryset(self):
-        """Return the last five published polls."""
-        return Poll.objects.order_by('-pub_date')[:5]
+        """Return the last five published polls. (excludes polls with with pub_at in the future"""
+        return Poll.objects.filter(
+            pub_date__lte=timezone.now()
+            ).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
     model = Poll
