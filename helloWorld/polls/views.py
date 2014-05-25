b8d1a230b708b31514d6a733a8ee74a62c747868
Marcus Fulbright <marcus.fulbright@iostudio.com>
2014-05-25 01:29:31 -0500

    implemented views and template to vote

--- helloWorld/polls/views.py
+++ helloWorld/polls/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.core.urlresolvers import reverse
 from django.template import RequestContext, loader
 
-from polls.models import Poll
+from polls.models import Poll, Choice
 
 def index(request):
     latest_poll_list = Poll.objects.order_by('-pub_date')[:5]
@@ -20,6 +21,21 @@ def results(request, poll_id):
     return HttpResponse("You're looking at the results of poll %s." % poll_id)
 
 def vote(request, poll_id):
-    return HttpResponse("You're voting on poll %s" % poll_id)
+    poll = get_object_or_404(Poll, pk=poll_id)
+    try:
+        selected_choice = poll.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render (request, 'polls/detail.html', {
+            'poll':poll,
+            'error_message': "Please make a choice",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(poll.id,)))
+
+def results(request, poll_id):
+    poll = get_object_or_404(Poll, pk=poll_id)
+    return render(request, 'polls/results.html', {'poll':poll})
 
 
