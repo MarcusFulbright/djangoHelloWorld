cc6e2f6550fac47cea7107675a5da3ec23adfe67
Marcus Fulbright <marcus.fulbright@iostudio.com>
2014-05-25 11:06:40 -0500

    created test case that describes a bug which allows polls with future published dates to have was_published_recently() return true

--- helloWorld/polls/tests.py
+++ helloWorld/polls/tests.py
@@ -1,3 +1,15 @@
+import datetime
+
+from django.utils import timezone
 from django.test import TestCase
 
-# Create your tests here.
+from polls.models import Poll
+
+class PollMethodTests(TestCase):
+    def test_was_published_recently_with_future_pub_date(self):
+        """
+        was_published_recently() should return False for polls if the pub_date is in the future
+        """
+        future_poll = Poll(pub_date=timezone.now() + datetime.timedelta(days=3))
+        self.assertEqual(future_poll.was_published_recently(), False)
+
